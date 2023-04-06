5b8ab8e625f058d5f7c6313ebb15b4e9dfa65fa4
kingForks <muhabdi47.com@gmail.com>
2023-04-06 10:03:21 +0800

    Add API Code Documentation

--- routes/api.php
+++ routes/api.php
@@ -26,14 +26,73 @@ Route::post('/change-password', [AuthController::class, 'change_password']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
+
+    /**
+     *  --------------------------------------
+     *          START - RENTBUILDING
+     *  --------------------------------------
+     */
+
+
+    /**
+     *  METHOD POST
+     *  BODY PARAMETER:
+     *      user_id, building_id, end_date(ahkir sewa), note
+     *
+     *  Untuk menyewakan gedung
+     */
     Route::post('/rent/building', [BuildingController::class, 'api_rent_building']);
+
+    /**
+     *  METHOD GET
+     *  URL PARAMETER:
+     *      field(user,building,rental), id
+     *
+     *  Untuk mendapatkan data gedung yang di-sewa dengan parameter yang di berikan
+     */
     Route::get('/get/rented/building/{field}/{id}', [BuildingController::class, 'api_get_rent_by_param']);
+
+    /**
+     *  METHOD POST
+     *  BODY PARAMETER:
+     *      payment_data, payment_method, rental_id, penalty
+     *      adm_fee, cost
+     *
+     *  Untuk membayar biaya/total sewa dari gedung
+     */
     Route::post('/pay/rent/building', [BuildingController::class, 'api_pay_rent']);
+
+    /**
+     *  METHOD DELETE
+     *  URL PARAMETER:
+     *      id
+     *
+     *  Untuk menghapus gedung yang di sewa pengguna
+     */
     Route::delete('/delete/rented/{id}/building/', [BuildingController::class, 'api_delete_rented_building']);
 
+    /**
+     *  METHOD GET
+     *  URL PARAMETER
+     *      id
+     *
+     *  Untuk mendapatkan data master gedung
+     */
     Route::get('/master/get/building/{id}', [BuildingController::class, 'api_get_building_by_id']);
+
+    /**
+     *  METHOD GET
+     *
+     *  Untuk mendapatkan semua data master gedung
+     */
     Route::get('/master/buildings', [BuildingController::class, 'api_get_all_buildings']);
 
+
+    /**
+     *  --------------------------------------
+     *            END - RENTBUILDING
+     *  --------------------------------------
+     */
+
+
 });
-
-
